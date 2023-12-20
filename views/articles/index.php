1d01f786a32fc10c946534d3ee32b48920cbd0d0
Mai Xuân Hiếu <02hieu02@gmail.com>
2023-12-20 13:03:28 +0700

    new push

--- views/articles/index.php
+++ views/articles/index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title>Article List</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha2/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBK2UABT4KIVLl5Oz1Nl9/jOrnZb1MU+dX4ePqHs3I9foEMyJ1k7b5D/xD" crossorigin="anonymous">
 </head>
 <body>
 <h1>Article List</h1>
@@ -29,5 +30,6 @@
     <?php endforeach; ?>
     </tbody>
 </table>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-GLhlTQ8iNl7lqyi8U5HR3kA12KO4P1foFhMShGmeCqF5iJqj8B+5AXdTEK1OQT5f" crossorigin="anonymous"></script>
 </body>
 </html>
